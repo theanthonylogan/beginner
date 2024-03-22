d9aa58c65ee2247827bff1a82c296d1e3918ff58
theanthonylogan <anthonyloganhall@gmail.com>
2024-03-22 05:14:48 +0000

    First inital beginner project commit

---
+++ coder/coder.py
@@ -0,0 +1,23 @@
+import string
+import random
+
+def main():
+    intro()
+    data = get_data_to_encode()
+    print(encode_data(data))
+
+def intro():
+    print("Welcome to the coder application. Please enter the phrase you want to encode and we will send back to you the encoded value.")
+
+def get_data_to_encode():
+    return input("Please enter data to encode: ")
+
+def encode_data(data):
+    encoded_data = ""
+    for x in data:
+        encoded_data += random.choice(string.ascii_letters)
+    return encoded_data
+
+
+if __name__ == "__main__":
+    main()

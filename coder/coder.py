a8b36cb84a2617b0b68cc8dcbe995b7fb96cc0ee
theanthonylogan <anthonyloganhall@gmail.com>
2024-03-22 14:02:00 +0000

    black files

--- coder/coder.py
+++ coder/coder.py
@@ -1,16 +1,22 @@
 import string
 import random
 
+
 def main():
     intro()
     data = get_data_to_encode()
     print(encode_data(data))
 
+
 def intro():
-    print("Welcome to the coder application. Please enter the phrase you want to encode and we will send back to you the encoded value.")
+    print(
+        "Welcome to the coder application. Please enter the phrase you want to encode and we will send back to you the encoded value."
+    )
+
 
 def get_data_to_encode():
     return input("Please enter data to encode: ")
+
 
 def encode_data(data):
     encoded_data = ""
